7bd6f9f9fad9961ce9d14a5d620915ce2d6fa22c
Kunal <khandelwal.kunal4@gmail.com>
2020-09-03 22:11:04 +0530

    Dockerized the app

--- assignment/assignment/settings.py
+++ assignment/assignment/settings.py
@@ -109,7 +109,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('redis', 6379)],
         },
     },
 }
@@ -120,9 +120,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bugbook',
-        'USER': '####',
-        'PASSWORD': '######',
-        'HOST': 'localhost', 
+        'USER': 'root',
+        'PASSWORD': 'mathspower',
+        'HOST': 'db', 
         'PORT': '3306',
 
     }
